b9e110dce221cfa56af446f6c8aac184e5611dee
luca.ganau <luca.ganau@certimeter.it>
2022-08-04 09:10:52 +0200

    textview sostituita da Text compose

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,85 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.databinding.FragmentPlantDetailBinding
 
-@Composable
-fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
+class PlantDetailDescription: Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<com.google.samples.apps.sunflower.databinding.FragmentPlantDetailBinding>(
+            inflater, R.layout.fragment_plant_detail, container, false
+        ).apply {
+            composeView.setContent {
+                // You're in Compose world!
+                MaterialTheme {
+                    PlantDetailDescription()
+                }
+            }
+        }
+
+        return binding.root
+    }
+
+    @Composable
+    fun PlantDetailDescription() {
+        Surface {
+            Text("Hello Compose")
+        }
+    }
+
+    @Composable
+    fun PlantName(name: String){
+        Text(
+            text = name,
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(R.dimen.margin_small))
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
+    }
+
+    //Parte per la preview
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun PreviewConversation() {
+        MaterialTheme {
+            PlantDetailDescription()
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun PlantNamePreview() {
+        MaterialTheme {
+            PlantName("Apple")
+        }
     }
 }
